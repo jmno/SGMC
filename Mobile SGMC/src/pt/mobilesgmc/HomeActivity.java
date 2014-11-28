cfd63a45a234eaf84d8ab48ca31c00dd70414bed
Nicolau <crbikers@gmail.com>
2014-11-28 21:28:26 +0000

    Menu with Gestures

--- Mobile SGMC/src/pt/mobilesgmc/HomeActivity.java
+++ Mobile SGMC/src/pt/mobilesgmc/HomeActivity.java
@@ -1,48 +1,98 @@
 package pt.mobilesgmc;
 
+import pt.mobilesgmc.modelo.OnSwipeTouchListener;
+import pt.mobilesgmc.view.viewgroup.FlyOutContainer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.mobilegsmc.R;
 
 public class HomeActivity extends Activity {
 
+	FlyOutContainer root;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+
 		
-		Button btnEquipa = (Button) findViewById(R.id.buttonEquipa);
-		btnEquipa.setOnClickListener(new OnClickListener() {
+		
+		
+		this.root = (FlyOutContainer) this.getLayoutInflater().inflate(R.layout.activity_home, null);
+		 Display display = getWindowManager().getDefaultDisplay();
+		    DisplayMetrics outMetrics = new DisplayMetrics ();
+		    display.getMetrics(outMetrics);
+
+		    float density  = getResources().getDisplayMetrics().density;
+		    float dpWidth  = outMetrics.widthPixels / density;
+		    int margin = (80*(int)dpWidth)/100;
+		root.setMargin(margin);
+		this.setContentView(root);
+		
+		
+		
+		root.setOnTouchListener(new OnSwipeTouchListener(this){
+			public void onSwipeTop() {
+		      //  Toast.makeText(SampleActivity.this, "top", Toast.LENGTH_SHORT).show();
+		    }
+		    public void onSwipeRight() {
+		    	String estado = root.getState().toString();
+		    	if(estado.equals("CLOSED"))
+		    	toggleMenu(findViewById(R.layout.activity_home));
+		      //  Toast.makeText(SampleActivity.this, "right", Toast.LENGTH_SHORT).show();
+		    }
+		    public void onSwipeLeft() {
+		    	String estado = root.getState().toString();
+		    	if(estado.equals("OPEN"))
+		    	toggleMenu(findViewById(R.layout.activity_home));
+		    }
+		    public void onSwipeBottom() {
+		       // Toast.makeText(SampleActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+		    }
+
+		public boolean onTouch(View v, MotionEvent event) {
+		    return gestureDetector.onTouchEvent(event);
+		}
 			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent equipa = new Intent(getBaseContext(),EquipaCirurgica.class);
-				startActivity(equipa);
-			}
+			
 		});
 		
-		Button btnUtentes = (Button) findViewById(R.id.buttonUtentes);
+		Button btnEquipa = (Button) root.findViewById(R.id.buttonEquipa);
+		btnEquipa.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent utentes = new Intent(getBaseContext(),
+						EquipaCirurgica.class);
+				toggleMenu(findViewById(R.layout.activity_home));
+				startActivity(utentes);
+				
+			}
+		});
+		Button btnUtentes = (Button) root.findViewById(R.id.buttonUtentes);
 		btnUtentes.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent utentes = new Intent(getBaseContext(),UtentesActivity.class);
+				Intent utentes = new Intent(getBaseContext(),
+						UtentesActivity.class);
+				toggleMenu(findViewById(R.layout.activity_home));
 				startActivity(utentes);
 			}
 		});
-				
-				
-				
-		
+
 	}
 
 	@Override
@@ -63,4 +113,11 @@ public class HomeActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public void toggleMenu(View v){
+		this.root.toggleMenu();
+	}
+	
+	
+
 }
